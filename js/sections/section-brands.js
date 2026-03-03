6df712c6cd4b1a8b75560a94094cfdeb33377994
Jens L. Hammer <jens@lindberghammer.dk>
2026-03-03 21:18:01 +0100

    Added brands section

--- js/sections/section-brands.js
+++ js/sections/section-brands.js
@@ -4,8 +4,25 @@ import { set } from "../utils/set.js";
 export function sectionBrands() {
   const section = create("section", "bg-bg");
 
-  const h2 = create("h2", "text-3xl text-secondary mb-4");
+  const h2 = create("h2", "text-3xl text-primary mb-4");
   h2.textContent = "Our Brands";
-  set(h2, section);
+
+  const grid = create("div", "grid grid-cols-2 md:grid-cols-3 gap-2");
+  const imgSrcs = [
+    "./assets/images/Brands-Green.jpg",
+    "./assets/images/Brands-Black.jpg",
+    "./assets/images/Brands-White.jpg",
+    "./assets/images/Brands-Rooibos.jpg",
+    "./assets/images/Brands-Herbal.jpg",
+    "./assets/images/Brands-Organic.jpg",
+  ];
+
+  imgSrcs.forEach((src) => {
+    const img = create("img", "w-full h-full object-cover");
+    img.src = src;
+    set(img, grid);
+  });
+
+  set([h2, grid], section);
   return section;
 }
